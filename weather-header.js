2a8b1113e079619930cecd35c6bab66ea937a612
Hpayeur <235752572+Hpayeur@users.noreply.github.com>
2025-12-17 13:29:44 -0700

    adding

--- weather-header.js
+++ weather-header.js
@@ -2,6 +2,11 @@
 
 const URL = 'https://api.openweathermap.org/data/2.5/weather?q=tooele&&units=imperial&appid=690b34bf536e8e5ed7ad088e82607116';
 
+const apiKey = '690b34bf536e8e5ed7ad088e82607116'; 
+const weatherDiv = document.getElementById('weather');
+const cityInput = document.getElementById('cityInput');
+const fetchWeatherBtn = document.getElementById('fetchWeatherBtn')
+
 fetch (URL)
 .then ((respone)=> respone.json())
 .then ((jsobject)=>{console.log (jsobject);
@@ -23,26 +28,19 @@ function removeLoadingMessage(){
         return response.json();
     })
 
-.then((jsobject)=>{
-    removeLoadingMessage();
-    console.log(jsobject);
-})
-
-function displayResults (jsobject){
-    document.getElementById('stats').textContent = jsobject.name;
-    document.getElementById('summary').textContent = jsobject.weather[0].description;
-    document.getElementById('current-temp').textContent = jsobject.main.temp;
-    document.getElementById('current-humid').textContent = jsobject.main.humidity;
-    document.getElementById('current-windSpeed').textContent = jsobject.wind.speed;
-}
-
+    .then((jsobject)=>{
+        removeLoadingMessage();
+        console.log(jsobject);
+    })
+// function displayResults (jsobject){
+//     document.getElementById('stats').textContent = jsobject.name;
+//     document.getElementById('summary').textContent = jsobject.weather[0].description;
+//     document.getElementById('current-temp').textContent = jsobject.main.temp;
+//     document.getElementById('current-humid').textContent = jsobject.main.humidity;
+//     document.getElementById('current-windSpeed').textContent = jsobject.wind.speed;
+// }
 
 
-
-
-
-
-// `https://api.openweathermap.org/data/2.5/weather?q=tooele&&units=imperial&appid=690b34bf536e8e5ed7ad088e82607116`
 // stats
 // summary
 // current-desc
